a4215ea2dc7239cfc644ba110ceea3975a5c3946
MohamedNabil114 <111948799+MohamedNabil114@users.noreply.github.com>
2022-12-25 23:18:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace broject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int S;
+            int R;
+            bool isPrime;
+
+            do
+            {
+                Console.Write("Enter start number: ");
+                S = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter range number: ");
+                R = int.Parse(Console.ReadLine());
+            }
+            while (R <= 1&&S>=2);
+            Console.WriteLine("All prime numbers between "+S+ " and " + R + ": ");
+            for (int p = S; p <= R; p++)
+            {
+                isPrime = true;
+                for (int k = 2; k <= p / 2; k++)
+                {
+                    if (p % k == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(p + " ");
+                }
+            }
+        }   
+    }
+}
